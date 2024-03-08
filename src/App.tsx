758f4a6d8215907099e8804cce87e37646b3e191
crystal1229d <crystal1229d@naver.com>
2024-03-08 23:19:45 +0900

    소셜 로그인 : Google 로그인

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import TestPage from '@pages/Test'
 import HotelListPage from '@pages/HotelList'
 import HotelPage from '@pages/Hotel'
 import useLoadKakao from './hooks/useLoadKakao'
+import SigninPage from './pages/Signin'
+import MyPage from './pages/My'
 
 function App() {
   useLoadKakao()
@@ -13,6 +15,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HotelListPage />} />
         <Route path="/hotel/:id" element={<HotelPage />} />
+        <Route path="/signin" element={<SigninPage />} />
+        <Route path="/my" element={<MyPage />} />
         <Route path="/test" element={<TestPage />} />
       </Routes>
     </BrowserRouter>
